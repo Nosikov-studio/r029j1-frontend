c8d5285182f8144c633c7498c78b7bc301a004e0
Nos <zvezdazanatih@gmail.com>
2025-05-27 14:03:37 +0700

    0

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+//************************************************************************* */
+const form1 = document.getElementById('form1');
+  form1.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+  const formData = new FormData(form1); // Сбор данных формы
+  const FormDataObject = Object.fromEntries(formData);
+
+  fetch('http://85.28.47.165:40444/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      },
+    body: JSON.stringify({
+      ...FormDataObject,
+      views: 0,
+    })
+  })
+    .then(response => response.json())
+    .then(j => {
+      console.log(j);      
+      form1.reset(); // очищаем форму
+    });
+  });
+
